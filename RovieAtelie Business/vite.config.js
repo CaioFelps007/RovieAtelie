6b314c779a5754558c6da2764a055962e6e4f83c
CaioFelps007 <125682381+CaioFelps007@users.noreply.github.com>
2025-10-06 23:48:21 -0300

    commit 2.1

--- RovieAtelie Business/vite.config.js
+++ RovieAtelie Business/vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "./", // importante para que funcione em Vercel
+  base: "./", // importante para deploy
+  build: {
+    outDir: "dist",
+  },
 });
